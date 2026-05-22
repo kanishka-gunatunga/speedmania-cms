cef22b7ec6512d8c5d472324c326f898fa4444e8
Kaveesha Sulakshana <sulakshanakaveesha@gmail.com>
2026-05-22 09:39:29 +0530

    fix: build errors

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -6,8 +6,12 @@ const globalForDb = globalThis as unknown as {
   conn: mysql.Pool | undefined;
 };
 
-const connectionString = process.env.DATABASE_URL!;
-const conn = globalForDb.conn ?? mysql.createPool(connectionString);
+const connectionString = process.env.DATABASE_URL;
+const conn = globalForDb.conn ?? (
+  connectionString 
+    ? mysql.createPool(connectionString) 
+    : mysql.createPool({ host: "localhost" })
+);
 
 if (process.env.NODE_ENV !== "production") globalForDb.conn = conn;
 
